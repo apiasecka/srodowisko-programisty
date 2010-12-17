e5be5c9c57d3e4d7e220cd35f646ef2a1c8b3b03
alexa <alexa1401@gmail.com>
2010-12-17 18:04:45 +0100

    zad 13 - lab4

---
+++ lab4_13.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+losuj=`python -c "import random; print random.randrange(1,50);"`
+echo $losuj
+while true; do
+
+echo "podaj liczbe"
+read liczba
+
+if [ $losuj -gt $liczba ] 
+then
+	echo "Za mala"
+elif [ $losuj -lt $liczba ]
+then
+	echo "Za duzo"
+elif [ $losuj -eq $liczba ]
+then
+	echo "OK"
+	exit
+fi
+done
